306223779cfe761e820fe11321e664d89db1d1a8
Jinsu Park <bo314@naver.com>
2020-12-03 12:04:27 +0900

    Add: doc

--- http/http.go
+++ http/http.go
@@ -1,7 +1,9 @@
+// freelancer의 http 통신과 관련된 내용을 담습니다.
+// 클라이언트들은 이 패키지의 문서를 통해 어떠한 형식의 데이터를
+// 전달해야하는지, 자신이 받는 데이터가 어떠한 형식인지를 파악할 수 있습니다.
 package http
 
 import (
-	"encoding/json"
 	"github.com/gorilla/websocket"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -11,20 +13,33 @@ import (
 	"strconv"
 )
 
+// websocket을 통해 클라이언트에게 메시지를 보낼 때 사용하는 타입.
+//
+// Type 은 주로 타입의 이벤트가 발생하였는지를 의미하고,
+// Data 는 현재 발생한 이벤트가 전달하고싶은 정보를 의미한다.
+// Data 에는 어떠한 자료형도 올 수 있지만, 전송될 때에는 JSON 형태로
+// 전송된다.
+//
+// 클라이언트는 어떤 이벤트 핸들러가 어떤 Type에 의해 어떤 동작을 할 지
+// 정의하고, 그 동작 시에 필요한 data는 Data에서 얻을 수 있다.
+type SocketMessage struct{
+	Type string
+	Data interface{} // 어떤 타입이든 가능
+}
+
+// Task나 Freelancer를 추가할 때 클라이언트가 보내주어야하는 
+// body 형식
 type AddRequest struct{
 	Number int
 }
 
-type SocketMessage struct{
-	Type string
-	Data interface{}
-}
-
 var (
-	upgrader = websocket.Upgrader{}
+	// 현재 연결되어있는 websocket connection들
 	WebsocketConnections = []*websocket.Conn{}
+	upgrader = websocket.Upgrader{}
 )
 
+// Echo 서버를 run할 수 있는 *echo.Echo type을 생성합니다.
 func NewEcho() *echo.Echo{
 	e := echo.New()
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
@@ -104,11 +119,11 @@ func wsHandler(c echo.Context) error {
 	for report := range office.Reports {
 		// Write
 		//data, err := json.Marshal(map[string]interface{}{"type":"message", "event": "report", "name": "report", "data": report, "endpoint": "/ws"})
-		data, err := json.Marshal(report)
-		if err != nil {
-			logrus.Error(err)
-		}
-		err = ws.WriteMessage(websocket.TextMessage, data)
+		//data, err := json.Marshal(report)
+		//if err != nil {
+		//	logrus.Error(err)
+		//}
+		err = ws.WriteJSON(SocketMessage{Type: "freelancer_state_report", Data: report})
 		if err != nil {
 			logrus.Println("A user has been disconnected")
 			return err
